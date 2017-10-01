2ab4fd465de7a65c09c36dc62aab1774059c4958
admin <Vivek.Jain3@cognizant.com>
2017-10-02 00:48:27 +0100

    added project to repo

---
+++ Human-Library/www/js/schooldata.js
@@ -0,0 +1,43 @@
+var humanlibdata = {"humanlib":[
+  {
+    "picture": "img/sujit.jpg",
+    "activity": "Coding for dummies",
+    "details":"Sunjith is a coder, a permanent, frequently borrowed “peoples”. He’s available to be borrowed every Tuesday between 1700 - 1900 at B&D Digilabs for Coding 101 (ages 7-70). ",
+    "Location":"0.3 mile away"
+  },
+  {
+    "picture": "img/Beginners-Piano.jpg",
+    "activity": "Piano for dummies",
+    "details":"I can teach people of any ages piano. I am a grade 7 player and believe that knowledge should be shared between two parties.",
+    "Location":"0.7 mile away"
+  },
+  {
+    "picture": "img/dscn9446.jpg",
+    "activity": "Cricket for Dummies",
+    "details":"I play cricket for the Indian national team. I am free from 16:00 - 18:30 on Tuesdays and Sundays.",
+    "Location":"0.3 mile away"
+  },
+  {
+    "picture": "img/plumber1.jpg",
+    "activity": "Plumbing",
+    "details":"I am a plumber and can be called out to repair dogdy toilets, kitchen faucets and leaks.",
+    "Location":"0.7 mile away"
+  },{
+    "picture": "img/F8111284-01.jpg",
+    "activity": "Rasberri Pi for dummies",
+    "details":"I can share my experence with Rasberri Pi's.",
+    "Location":"0.3 mile away"
+  },
+  {
+    "picture": "img/GGBOB307118-1.jpg",
+    "activity": "Crumblebot for dummies",
+    "details":"I can share my experence with Crumblebot Coding",
+    "Location":"0.7 mile away"
+  },
+  {
+    "picture": "img/Sun-Java-JDK_1.jpg",
+    "activity": "Java Coding for dummies",
+    "details":"I can share my experence with Java Coding",
+    "Location":"0.3 mile away"
+  }
+]}
